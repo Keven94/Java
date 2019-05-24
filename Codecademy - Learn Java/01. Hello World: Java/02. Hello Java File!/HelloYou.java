ef6c49a430b1cbccc3e36937a56a521bd2fe9831
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-23 20:54:39 -0500

    Create HelloYou.java

---
+++ Codecademy - Learn Java/01. Hello World: Java/02. Hello Java File!/HelloYou.java
@@ -0,0 +1,15 @@
+/*
+The text editor has a file, HelloYou.java, that contains a HelloYou class with a main() method.
+
+Inside main(), add a statement which prints Hello someName!, with your name replacing someName.
+
+For example, if your name were “Maria,” the program would print Hello Maria!.
+*/
+
+public class HelloYou 
+{
+  public static void main(String[] args) 
+  {
+  	System.out.println("Hello Kevin!");
+  }
+}
