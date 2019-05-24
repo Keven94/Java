9aec90a38e2067f74b8bd26bb67e05a9996b8c26
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-23 22:31:10 -0500

    Create Char.java

---
+++ Codecademy - Learn Java/02. Variables and Data Types/05. char/Char.java
@@ -0,0 +1,16 @@
+/*
+1. Create a variable called expectedGrade of type char.
+Fill it with a single letter, representing the grade you think you would get in a graded Java course where the grades A, B, C, D and F are possible.
+
+2. Print out your expectedGrade variable!
+*/
+
+public class char
+{
+  public static void main (String[] args)
+  {
+    char expectedGrade = 'A';
+    
+    System.out.println(expectedGrade);
+  }
+}
