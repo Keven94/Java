3377bde12c635c20d0310ed6d26a641aee043462
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-23 21:27:56 -0500

    Create LanguageFacts2.java

---
+++ Codecademy - Learn Java/01. Hello World: Java/04. Semicolons and Whitespace/LanguageFacts2.java
@@ -0,0 +1,19 @@
+/*
+Inside main(), add a new statement printing how you feel about coding.
+
+Start the message with: “Programming is… “.
+
+Remember to place a semicolon at the end of the statement!
+*/
+
+public class LanguageFacts 
+{
+  public static void main(String[] args) 
+  {
+    // Press enter or return on your keyboard after each semicolon!
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+    System.out.println("Programming uses the same logic no matter the language");
+  }
+}
