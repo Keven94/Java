c1e89d98bca53f33349cd28fa8d5863be0fea483
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-23 22:31:42 -0500

    Update Booleans.java

--- Codecademy - Learn Java/02. Variables and Data Types/04. booleans/Booleans.java
+++ Codecademy - Learn Java/02. Variables and Data Types/04. booleans/Booleans.java
@@ -1,5 +1,6 @@
 /*
-1.Create a variable called intsCanHoldDecimals. Set it to true if the int type can hold a decimal number. Set it to false if the int type cannot do this.
+1.Create a variable called intsCanHoldDecimals. 
+Set it to true if the int type can hold a decimal number. Set it to false if the int type cannot do this.
 
 2.Print out your intsCanHoldDecimals variable.
 */
