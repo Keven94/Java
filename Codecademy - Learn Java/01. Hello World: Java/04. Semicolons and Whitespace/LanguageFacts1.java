f5d9f3d8548332af6aecb4c08948108d7e7ba3f7
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-23 21:24:25 -0500

    Create LanguageFacts1.java

---
+++ Codecademy - Learn Java/01. Hello World: Java/04. Semicolons and Whitespace/LanguageFacts1.java
@@ -0,0 +1,18 @@
+/*
+The LanguageFacts.java file prints information about Java to the screen.
+
+Unfortunately, the writer of the file has avoided using whitespace.
+
+Make the file easier to read by adding a newline after each statement!
+*/
+
+public class LanguageFacts 
+{
+  public static void main(String[] args) 
+  {
+    // Press enter or return on your keyboard after each semicolon!
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+  }
+}
