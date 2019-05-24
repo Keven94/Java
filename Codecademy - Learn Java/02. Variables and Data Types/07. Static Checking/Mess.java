37f804b1209dc05922e6fd27e7c0a500004d94f1
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-24 09:05:07 -0500

    Create Mess.java

---
+++ Codecademy - Learn Java/02. Variables and Data Types/07. Static Checking/Mess.java
@@ -0,0 +1,17 @@
+/*
+Change the types of the variables so that they correspond with the type of the assignment values.
+
+For example, year is assigned 2001, so it should be an int.
+*/
+
+public class Mess 
+{
+	public static void main(String[] args) 
+  {   
+		int year = 2001;
+    String title = "Shrek";
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
+	}
+}
