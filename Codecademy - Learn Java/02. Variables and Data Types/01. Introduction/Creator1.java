05133c3231d2dd477fb980f8390961d4858429f4
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-23 22:08:32 -0500

    Update Creator1.java

--- Codecademy - Learn Java/02. Variables and Data Types/01. Introduction/Creator1.java
+++ Codecademy - Learn Java/02. Variables and Data Types/01. Introduction/Creator1.java
@@ -4,6 +4,7 @@ In Creator.java, we have defined some variables related to James Gosling, the cr
 1.Inside main(), use System.out.println() to print out the variable name.
 2.Use the same command to print out yearCreated.
 */
+
 public class Creator 
 {
 	public static void main(String[] args) 
