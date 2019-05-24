cdfb8b6de6e06b32f64257e6d2e576404ee241e2
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-23 22:05:08 -0500

    Create Creator1.java

---
+++ Codecademy - Learn Java/02. Variables and Data Types/01. Introduction/Creator1.java
@@ -0,0 +1,16 @@
+/*
+In Creator.java, we have defined some variables related to James Gosling, the creator of Java.
+
+Inside main(), use System.out.println() to print out the variable name.
+*/
+
+public class Creator 
+{
+	public static void main(String[] args) 
+  {
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    
+    System.out.println(name);
+	}
+}
