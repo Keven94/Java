49ff61c54cc5be8f4616b6a743b66843a70e6229
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-25 13:54:58 -0500

    Update MultAndDivide.java

--- Codecademy - Learn Java/03. Manipulating Variables/03. Multiplication and Division/MultAndDivide.java
+++ Codecademy - Learn Java/03. Manipulating Variables/03. Multiplication and Division/MultAndDivide.java
@@ -11,16 +11,16 @@ Print the perPerson variable!
 public class MultAndDivide 
 {
 	public static void main(String[] args) 
-  {   
+  	{   
 		double subtotal = 30;
-    double tax = 0.0875;
+    		double tax = 0.0875;
     
-    double total = subtotal + (subtotal * tax);
+    		double total = subtotal + (subtotal * tax);
     
-    System.out.println(total);
+    		System.out.println(total);
     
-    double perPerson = total / 4;
+    		double perPerson = total / 4;
     
-    System.out.println(perPerson);
+    		System.out.println(perPerson);
 	}
 }
