34a6faf9679d79ec9fb4fda1ae24d29fd6958b8c
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-25 14:37:54 -0500

    Update GreatLessThan.java

--- Codecademy - Learn Java/03. Manipulating Variables/05. Greater Than and Less Than/GreatLessThan.java
+++ Codecademy - Learn Java/03. Manipulating Variables/05. Greater Than and Less Than/GreatLessThan.java
@@ -5,16 +5,18 @@
 Print out whether creditsToGraduate is less than creditsAfterSeminar.
 */
 
-public class GreaterLessThan {
-	public static void main(String[] args) {   
+public class GreaterLessThan 
+{
+	public static void main(String[] args) 
+	{   
 		double creditsEarned = 176.5;
-    double creditsOfSeminar = 8;
-    double creditsToGraduate = 180;
+    		double creditsOfSeminar = 8;
+    		double creditsToGraduate = 180;
     
-    System.out.println(creditsEarned > creditsToGraduate);
+    		System.out.println(creditsEarned > creditsToGraduate);
     
-    double creditsAfterSeminar = creditsEarned + creditsOfSeminar;
+    		double creditsAfterSeminar = creditsEarned + creditsOfSeminar;
     
-    System.out.println(creditsToGraduate < creditsAfterSeminar);
+    		System.out.println(creditsToGraduate < creditsAfterSeminar);
 	}
 }
