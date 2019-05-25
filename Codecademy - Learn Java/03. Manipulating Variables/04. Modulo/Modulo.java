675c2c0eed7c4571bb1716d9254a4b396b6a5bbe
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-25 14:00:08 -0500

    Update Modulo.java

--- Codecademy - Learn Java/03. Manipulating Variables/04. Modulo/Modulo.java
+++ Codecademy - Learn Java/03. Manipulating Variables/04. Modulo/Modulo.java
@@ -7,10 +7,10 @@ Create a variable called leftOut that holds the modulo of students and 3. Then, 
 public class Modulo 
 {
 	public static void main(String[] args) 
-  {   
+  	{   
 		int students = 26;
-    int leftOut = students % 3;
+    		int leftOut = students % 3;
     
-    System.out.println(leftOut);
+    		System.out.println(leftOut);
 	}
 }
