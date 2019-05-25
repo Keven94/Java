8d3960bff7544bc7c928189ff2ba1473a51f9b3b
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-24 22:03:06 -0500

    Create MyProfile.java

---
+++ Codecademy - Learn Java/02. Variables and Data Types/09. Review/MyProfile.java
@@ -0,0 +1,24 @@
+/*
+1.The file MyProfile.java contains a class that represents your hiring profile as presented to potential employers.
+In the main() method, create a variable called name that holds your name, as a sequence of characters.
+
+2.Create a variable called age that holds your age as a whole number.
+
+3.Create a variable called desiredSalary that holds your desired salary per year to a precision of two decimal points.
+
+4.Create a variable called gender that holds a single character, m (male), f (female), n (for none), or o (for other).
+
+5.Create a variable called lookingForJob that holds whether or not you are currently open to job offers.
+*/
+
+public class MyProfile 
+{
+	public static void main(String[] args) 
+  {
+    String name = "Kevin";
+    int age = 24;
+    double desiredSalary = 80000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+  }
+}
