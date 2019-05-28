611496ea4513fbe70771bcbf1df5bc134383ffa1
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-28 03:04:39 -0500

    Create Store.java

---
+++ Codecademy - Learn Java/04. Introduction to Classes/05. Constructor Parameters/Store.java
@@ -0,0 +1,24 @@
+/*
+1. Add the String parameter product to the Store() constructor.
+
+2. Inside of the constructor method, set the instance variable productType equal to the product parameter.
+*/
+
+public class Store
+{
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store(String product)
+  {
+    productType = product;
+  }
+  
+  // main method
+  public static void main(String[] args)
+  {
+    
+    
+  }
+}
