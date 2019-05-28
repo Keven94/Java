d174869e7035858ff00e03ce92d7a68bc2b80f09
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-28 01:49:34 -0500

    Create Store.java

---
+++ Codecademy - Learn Java/04. Introduction to Classes/04. Instance Fields/Store.java
@@ -0,0 +1,26 @@
+/*
+1. Add some state to our Store class.
+Declare a String instance field for productType.
+*/
+
+public class Store
+{
+  // declare instance fields here!
+  String productType;
+  
+  // constructor method
+  public Store()
+  {
+    System.out.println("I am inside the constructor method.");
+  }
+  
+  // main method
+  public static void main(String[] args)
+  {
+    System.out.println("This code is inside the main method.");
+    
+    Store lemonadeStand = new Store();
+    
+    System.out.println(lemonadeStand);
+  }
+}
