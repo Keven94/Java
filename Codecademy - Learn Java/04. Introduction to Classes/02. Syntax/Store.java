2b18451a1bfbc2621b35d1e6ceb324ce6a0231d2
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-27 23:46:06 -0500

    Create Store.java

---
+++ Codecademy - Learn Java/04. Introduction to Classes/02. Syntax/Store.java
@@ -0,0 +1,14 @@
+/*
+1. In the code editor, create a public Store class.
+
+2. Your program will not compile without a main() method.
+Define one within Store.
+*/
+
+public class Store 
+{
+  public static void main(String[] args)
+  {
+    
+  }
+}
