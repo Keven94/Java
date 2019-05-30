d42761bf1d13c6ab8bab77fd4283e149b4c2574a
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-29 22:54:55 -0500

    Update Precedence.java

--- Codecademy - Learn Java/06. Conditionals and Control Flow/05. Boolean Operators: Precedence/Precedence.java
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/05. Boolean Operators: Precedence/Precedence.java
@@ -2,8 +2,8 @@
 Line 10 has a code statement that is incomplete. 
 Use each Boolean operator no more than once to replace the empty comments /**/. 
 //The code statement should print out false.
-*/
 
+	
 public class Precedence {
 	public static void main(String[] args) {
 
