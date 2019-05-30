ee779bb657ba62f10787abc1c30c8ad1f9f5582e
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-29 23:00:08 -0500

    Create If.java

---
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/06. If Statement/If.java
@@ -0,0 +1,16 @@
+/*
+The if statement in the code editor is missing its Boolean expression. 
+Provide the if statement with a Boolean expression that evaluates to true.
+*/
+
+public class If {
+	public static void main(String[] args) {
+
+		if (1 > 0) {
+
+			System.out.println("Access granted.");
+
+		}
+		
+	}
+}
