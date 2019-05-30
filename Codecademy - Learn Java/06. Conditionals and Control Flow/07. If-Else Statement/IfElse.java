6c7cfc7e4426f8089e7e1c7cbe1e83210ffa0cde
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-29 23:56:53 -0500

    Create IfElse.java

---
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/07. If-Else Statement/IfElse.java
@@ -0,0 +1,20 @@
+/*
+The if/else statement in the code editor currently prints out the code in the if block. 
+Modify the if statement’s Boolean expression so that the code in the else block gets executed and prints to the console.
+*/
+
+public class IfElse {
+	public static void main(String[] args) {
+
+		if (8 <= 7) {
+
+			System.out.println("Try again...");
+
+		} else {
+
+			System.out.println("Success!");
+
+		}
+		
+	}
+}
