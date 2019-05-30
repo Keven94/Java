e3a4bc3c6b52191cb4d5341369434a11542b8698
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-29 21:52:47 -0500

    Create Not.java

---
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/04. Boolean Operators: !/Not.java
@@ -0,0 +1,14 @@
+/*
+1. Use the ! operator to return a value of true for the code on line 10.
+
+2. Use the ! operator to return a value of false for the code on line 11.
+*/
+
+public class Not {
+	public static void main(String[] args) {
+
+		System.out.println(!false);
+		System.out.println( !(5>=1) );
+		
+	}
+}
