55f442d7cde6678618bc9756979850883534f57f
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-30 00:00:07 -0500

    Create IfElseIf.java

---
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/08. If-ElseIf-Else Statement/IfElseIf.java
@@ -0,0 +1,22 @@
+//Set the value of the round variable so that the code in the else if block runs.
+
+public class IfElseIf {
+	public static void main(String[] args) {
+
+		int round = 1;
+
+		if (round > 12) {
+
+			System.out.println("The match is over!");
+
+		} else if (round > 0) {
+
+			System.out.println("The match is underway!");
+
+		}	else {
+
+			System.out.println("The boxing match hasn't started yet.");
+
+		}	
+	}
+}
