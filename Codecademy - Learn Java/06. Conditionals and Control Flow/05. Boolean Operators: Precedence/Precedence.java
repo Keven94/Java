3773c3a5343f3f64a465455472448d18e41165ee
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-29 22:53:52 -0500

    Create Precedence.java

---
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/05. Boolean Operators: Precedence/Precedence.java
@@ -0,0 +1,14 @@
+/*
+Line 10 has a code statement that is incomplete. 
+Use each Boolean operator no more than once to replace the empty comments /**/. 
+The code statement should print out false.
+*/
+
+public class Precedence {
+	public static void main(String[] args) {
+
+		boolean riddle = !( 1 < 8 && (5 > 2 || 3 < 5));
+		System.out.println(riddle);
+
+	}
+}
