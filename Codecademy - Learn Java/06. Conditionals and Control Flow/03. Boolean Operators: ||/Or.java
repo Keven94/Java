4badbf0760254284b6fe559257b737a71ce62d6a
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-29 21:50:19 -0500

    Create Or.java

---
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/03. Boolean Operators: ||/Or.java
@@ -0,0 +1,14 @@
+/*
+Use the || operator and any two Boolean expressions of your choice between the parentheses of System.out.println(); 
+in order to print out a value of true or false to the console.
+*/
+
+public class Or 
+{
+	public static void main(String[] args) 
+  {
+
+		System.out.println(true || false);
+		
+	}
+}
