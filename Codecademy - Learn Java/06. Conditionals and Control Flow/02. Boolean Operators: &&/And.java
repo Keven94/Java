5937134b6e2e715ec8c0be2b204827b7f492bfab
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-29 21:47:11 -0500

    Create And.java

---
+++ Codecademy - Learn Java/06. Conditionals and Control Flow/02. Boolean Operators: &&/And.java
@@ -0,0 +1,12 @@
+/*
+Use the && operator and any two Boolean expressions of your choice between the parentheses of System.out.println(); 
+in order to print out a value of true or false to the console.
+*/
+
+public class And 
+{
+	public static void main(String[] args) 
+  {
+		System.out.println(true && false);		
+	}
+}
