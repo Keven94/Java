b42e060ba367d69943eb556e0c7ef2084b91e5b9
Kevin Ibarra <40154212+Keven94@users.noreply.github.com>
2019-06-03 12:25:33 -0500

    Create FromHundredToOne.java

---
+++ MOOC - Object-Oriented programming with Java/Part 01/Week2/Exercise 28: From hundred to one/FromHundredToOne.java
@@ -0,0 +1,13 @@
+public class FromHundredToOne
+{
+    public static void main(String[] args)
+    {
+        // Write your program here
+        int integer = 100;
+        while(integer > 0)
+        {
+            System.out.println(integer);
+            integer--;
+        }
+    }
+}
