da46984ff94cd8faf6c73dbe9af9c7e1a2a1823f
Kevin Ibarra <40154212+Keven94@users.noreply.github.com>
2019-06-06 04:07:26 -0500

    Add files via upload

---
+++ MOOC - Object-Oriented programming with Java/Part 01/Week 2/Exercise 27_ From one to a hundred/FromOneToHundred.java
@@ -0,0 +1,13 @@
+public class FromOneToHundred
+{
+    public static void main(String[] args)
+    {
+        // Write your program here
+        int integer = 1;
+        while(integer < 101)
+        {
+            System.out.println(integer);
+            integer++;
+        }
+    }
+}
