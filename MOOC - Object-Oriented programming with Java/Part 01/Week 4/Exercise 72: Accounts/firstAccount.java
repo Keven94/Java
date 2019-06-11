102588d76ad4da6457800fcbfe7e8b9dc4fab299
Kevin Ibarra <40154212+Keven94@users.noreply.github.com>
2019-06-11 00:59:37 -0500

    Update firstAccount.java

--- MOOC - Object-Oriented programming with Java/Part 01/Week 4/Exercise 72: Accounts/firstAccount.java
+++ MOOC - Object-Oriented programming with Java/Part 01/Week 4/Exercise 72: Accounts/firstAccount.java
@@ -1,20 +1,14 @@
+import java.util.Scanner;
+
 public class Accounts 
 {
-    public static void transfer(Account from, Account to, double howMuch)
-    {
-        from.withdrawal(howMuch);
-        to.deposit(howMuch);
-    }
-    
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         // Code in Account.Java should not be touched!
         // write your code here
-        Account a = new Account("A", 100.0);
-        Account b = new Account("B", 0.0);
-        Account c = new Account("C", 0.0);
-        
-        transfer(a, b, 50.0);
-        transfer(b, c, 25.0);
+        Account firstAccount = new Account("First acount", 100.0);
+        firstAccount.deposit(20.0);
+        System.out.println(firstAccount);
     }
+
 }
